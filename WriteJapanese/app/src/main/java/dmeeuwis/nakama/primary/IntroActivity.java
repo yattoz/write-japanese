5607c011fff37a4614ba650fcd50d21efc61a3fe
David Meeuwis <dmeeuwis@gmail.com>
2017-07-14 21:31:19 -0700

    Working on intro activity with movies

--- WriteJapanese/app/src/main/java/dmeeuwis/nakama/primary/IntroActivity.java
+++ WriteJapanese/app/src/main/java/dmeeuwis/nakama/primary/IntroActivity.java
@@ -2,8 +2,10 @@ package dmeeuwis.nakama.primary;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
 
 import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.MessageButtonBehaviour;
 import agency.tango.materialintroscreen.SlideFragmentBuilder;
 import agency.tango.materialintroscreen.VideoSlideFragment;
 import dmeeuwis.kanjimaster.R;
@@ -16,37 +18,36 @@ public class IntroActivity extends MaterialIntroActivity {
         super.onCreate(savedInstanceState);
 
         addSlide(VideoSlideFragment.createInstance(R.color.intro_blue, R.color.LightBlue, R.raw.correct_draw,
-                        "Welcome to Write Japanese",
-                        "Learn to draw the Japanese writing systems: Hiragana, Katakana, and the Kanji.\n\nWrite Japanese uses a time-based repetition (aka. spaced repetition system) to manage your learning. It helps you study the right characters, when you need to."
-                ));
+                "Welcome to Write Japanese",
+                "Learn to draw the Japanese writing systems: Hiragana, Katakana, and the Kanji.\n\nWrite Japanese uses a time-based repetition (aka. spaced repetition system) to manage your learning. It helps you study the right characters, when you need to."
+        ));
 
-        addSlide(new SlideFragmentBuilder()
-            .backgroundColor(R.color.intro_teal)
-                .buttonsColor(R.color.LightBlue)
-            .title("Draw the Character")
-            .description("At the main screen, draw the character indicated in the instruction card. You can choose to be prompted based on character meaning, readings, or vocab examples.")
-            .build());
+        addSlide(VideoSlideFragment.createInstance(R.color.intro_teal, R.color.LightBlue, R.raw.correct_draw,
+                "Draw the Character",
+                "Draw the character indicated in the instruction card. You can choose to be prompted based on character meaning, readings, or vocab examples."
+        ));
 
-        addSlide(new SlideFragmentBuilder()
-                .backgroundColor(R.color.intro_green)
-                .buttonsColor(R.color.LightBlue)
-                .title("You got it!")
-                .description("If you've drawn correctly, you'll see a green screen with some example usages of the character. Then continue on to the next!")
-                .build());
+        addSlide(VideoSlideFragment.createInstance(R.color.intro_green, R.color.LightBlue, R.raw.correct_draw,
+                "You got it!",
+                "If you've drawn correctly, you'll see a happy green screen with vocab examples of the character. Then continue on to the next!"));
 
-        addSlide(new SlideFragmentBuilder()
-                .backgroundColor(R.color.intro_red)
-                .buttonsColor(R.color.LightBlue)
-                .title("Not so fast!")
-                .description("If incorrect, you'll have a chance to review in study mode. Trace the character, and make a story to help you remember it for next time.")
-                .build());
+        addSlide(VideoSlideFragment.createInstance(R.color.intro_red, R.color.LightBlue, R.raw.incorrect_demo,
+                "Not so fast!",
+                "If incorrect, you'll see an angry red screen, and have a chance to review in study mode. Trace the character, and make a story to help you remember it for next time."));
 
         addSlide(new SlideFragmentBuilder()
                 .backgroundColor(R.color.intro_teal)
                 .buttonsColor(R.color.LightBlue)
                 .title("Across your devices")
                 .description("To sync your progress across all your devices - or save your progress if you lose your device - click the below button to enable cross device sync.\n\nYou may be prompted to select a Google account.")
-                .build());
+                .possiblePermissions(new String[] { Manifest.permission.ACCOUNT_MANAGER } )
+                .build(),
+                new MessageButtonBehaviour(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMessage("We provide solutions to make you love your work");
+                    }
+                }, "Enable Cross Device Sync"));
 
         addSlide(new SlideFragmentBuilder()
                 .backgroundColor(R.color.intro_green)
