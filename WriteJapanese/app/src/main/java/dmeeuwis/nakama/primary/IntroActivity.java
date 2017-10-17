5c2d36fb9f2dfc05afd64608c6cb11d4c2129df4
David Meeuwis <dmeeuwis@gmail.com>
2017-10-16 21:25:57 -0700

    Hacking around MaterialIntro color bug

--- WriteJapanese/app/src/main/java/dmeeuwis/nakama/primary/IntroActivity.java
+++ WriteJapanese/app/src/main/java/dmeeuwis/nakama/primary/IntroActivity.java
@@ -57,11 +57,23 @@ public class IntroActivity extends MaterialIntroActivity implements View.OnClick
         // show SRS screen if not yet shown, or if specifically requested
         boolean srsNotYetShow = Settings.getBooleanSetting(getApplicationContext(), USE_SRS_SETTING_NAME, null) == null;
         boolean srsRequested = intent != null && intent.getBooleanExtra(REQUEST_SRS_SETTINGS, false);
+        boolean showSrs = srsNotYetShow || srsRequested;
 
-        if(srsNotYetShow || srsRequested){
+        Settings.SyncStatus syncStatus = Settings.getCrossDeviceSyncEnabled(getApplicationContext());
+        boolean syncRequested = intent != null && intent.getBooleanExtra(REQUEST_SYNC_SETTINGS, false);
+        boolean showSync = !syncStatus.asked || syncRequested;
+
+        int slideCount = (showSrs ? 1 : 0) + (showSync ? 1 : 0);
+
+        if(showSrs){
             Log.i("nakama-intro", "Showing srs screen: " + srsNotYetShow + ", " + srsRequested);
             SlideFragment s =
-                CheckboxSlideFragment.createInstance(r.getColor(R.color.intro_green), r.getColor(R.color.intro_blue), R.drawable.calendar2,
+                CheckboxSlideFragment.createInstance(
+                        slideCount == 1 ? r.getColor(R.color.intro_green) : R.color.intro_green,
+                        slideCount == 1 ? r.getColor(R.color.intro_blue) : R.color.intro_blue,
+                        //r.getColor(R.color.intro_green),
+                        //r.getColor(R.color.intro_blue),
+                        R.drawable.calendar2,
                         "Spaced Repetition",
                         "Once written correctly a few times, characters will be repeated at increasing timed intervals, across many days, to really lock them in your memory. The built-in schedule repeats after 1, 3, 7, 14, and 30 days.",
                         "Use Spaced Repetition", USE_SRS_SETTING_NAME,
@@ -79,14 +91,14 @@ public class IntroActivity extends MaterialIntroActivity implements View.OnClick
             }
         }
 
-        Settings.SyncStatus syncStatus = Settings.getCrossDeviceSyncEnabled(getApplicationContext());
-        boolean syncRequested = intent != null && intent.getBooleanExtra(REQUEST_SYNC_SETTINGS, false);
 
-        if(!syncStatus.asked || syncRequested) {
+        if(showSync) {
             Log.i("nakama-intro", "Showing sync status screen: " + !syncStatus.asked + ", " + syncRequested);
             SlideFragment s = ButtonSlideFragment.createInstance(
-                            r.getColor(R.color.intro_teal),
-                            r.getColor(R.color.intro_blue),
+                            //r.getColor(R.color.intro_teal),
+                            //r.getColor(R.color.intro_blue),
+                            slideCount == 1 ? r.getColor(R.color.intro_teal) : R.color.intro_teal,
+                            slideCount == 1 ? r.getColor(R.color.intro_blue) : R.color.intro_blue,
                             R.drawable.device_sync,
                             "Across your devices",
                             "To sync your progress across all your Android devices - or save your progress if you lose your device - click the below button to enable cross-device sync.\n\nYou may be prompted to select which Google account to sync across.",
