5a3d2f48f87ea7c91ac318136c60cdffd8b30e2b
David Meeuwis <dmeeuwis@gmail.com>
2019-05-31 22:41:21 -0700

    Improve purchase dialog text

--- WriteJapanese/app/src/main/java/dmeeuwis/nakama/views/PurchaseDialog.java
+++ WriteJapanese/app/src/main/java/dmeeuwis/nakama/views/PurchaseDialog.java
@@ -14,17 +14,17 @@ public class PurchaseDialog extends DialogFragment {
 	private final static String MESSAGE_KEY = "message_key";
 	
     public enum DialogMessage { 
-    							LOCK_BUTTON("The katakana and higher level kanji sets are locked, and require a one-time purchase to unlock. 10 characters are available from each set as a sample.\n\nUnlocking will give you access to standardSets characters, and support the further development of this application."),
-    							START_OF_LOCKED_SET("The katakana and higher level kanji sets are locked, and require a one-time purchase to unlock. 10 characters are available from each set as a sample.\n\nUnlocking will give you access to standardSets characters, and support the further development of this application."),
-    							END_OF_LOCKED_SET("You reached the end of the sample characters for this set. To access the complete set, a one-time unlock is required.\n\nUnlocking will give you access to standardSets characters, and support the further development of this application."),
-    							LOCKED_CHARACTER("This character is part of the locked set, and requires a one-time purchase to unlock. This purchase will unlock standardSets characters, and support the further development of this application.");
+    							LOCK_BUTTON("The katakana and higher level kanji sets are locked, and require a one-time purchase to unlock. 10 characters are available from each set as a sample.\n\nUnlocking will give you access to all 2400 characters, and support the further development of this application."),
+    							START_OF_LOCKED_SET("The katakana and higher level kanji sets are locked, and require a one-time purchase to unlock. 10 characters are available from each locked set as a sample.\n\nUnlocking will give you access to all 2400 characters, and support the further development of this application."),
+    							END_OF_LOCKED_SET("You reached the end of the sample characters for this set. To access the complete set, a one-time unlock is required.\n\nUnlocking will give you access to all 2400 characters, and support the further development of this application."),
+    							LOCKED_CHARACTER("This character is part of the locked set, and requires a one-time purchase to unlock. The purchase will unlock all 2400 characters, and support the further development of this application.");
 
     							public final String message;
     
     							DialogMessage(String message){
     								this.message = message;
     							}
-    				};
+    				}
     				
     public static PurchaseDialog make(DialogMessage mess){
     	Bundle b = new Bundle();
