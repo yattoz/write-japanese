d3627e10fe87c2ba49954d3587182ca1743444ca
yattoz <12408848+yattoz@users.noreply.github.com>
2025-11-06 00:56:07 +0000

    basic reading

--- Gemfile
+++ Gemfile
@@ -1,6 +1,12 @@
 source 'https://rubygems.org'
 
+
+gem 'irb'
+gem 'rdoc'
+gem 'fiddle'
 # note: to install pry on Windows, you need to install MSYS2 with the SDK.
 # run "ridk install" and press 3.
 gem 'pry'
+
 gem 'nokogiri'
+gem 'ruby-lsp'
--- JMDictAssetsBuilder/read_jmdict.rb
+++ JMDictAssetsBuilder/read_jmdict.rb
@@ -1,5 +1,26 @@
 require 'nokogiri'
-
-doc = Nokogiri::XML(File.open("JMDict.xml")) do |config|
+require 'pry'
+doc = Nokogiri::XML(File.open("JMDictAssetsBuilder/JMDict.xml")) do |config|
   config.strict.noblanks
 end
+
+entries = doc.xpath("//entry")
+nicechar = '之'
+selected = []
+entries.each do |unit|
+  k_ele = unit.css("k_ele")
+  if !k_ele.empty?
+    keb = k_ele.css("keb")
+      if !keb.empty?
+        if keb.first.text.include?(nicechar)
+           selected.append(unit)
+           puts "Found #{keb.first.text}"
+        end
+      end
+  end
+end
+puts selected.size
+
+
+
+
